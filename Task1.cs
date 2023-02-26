68e4ef9d066f606644d26ea22e9d205e43938a83
Wild Mosquito <nailnh@code.edu.az>
2023-02-26 12:17:42 +0400

    tasks

---
+++ Task1.cs
@@ -0,0 +1,29 @@
+﻿#region task1
+Console.WriteLine("Enter your exam result:");
+int pts = int.Parse(Console.ReadLine());
+
+if (pts >= 0 && pts < 51)
+{
+    Console.WriteLine("Your grade is F, You have failed");
+}else if (pts >= 51 && pts < 61)
+{
+    Console.WriteLine("Your grade is E");
+}else if(pts >= 61 && pts < 71)
+{
+    Console.WriteLine("Your grade is D");
+}else if (pts >= 71 && pts < 81)
+{
+    Console.WriteLine("Your grade is C");
+}else if (pts >= 81 && pts < 91)
+{
+    Console.WriteLine("Your grade is B");
+}else if (pts >= 91 && pts <= 100)
+{
+    Console.WriteLine("Your grade is A");
+}else
+{
+    Console.WriteLine("Enter a valid amount of points");
+}
+
+#endregion 
+//Fun fact: "E" eslinde yoxdur :)
---
+++ Task3.cs
@@ -0,0 +1,13 @@
+﻿#region task3
+int[] arr = { 1, 2, 3, 8, 5 };
+int biggest = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > biggest)
+    {
+        biggest = arr[i];
+    }
+}
+Console.WriteLine(biggest);
+#endregion
